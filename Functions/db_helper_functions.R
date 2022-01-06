006f3a52c8117685e62d4af7ae3f998da34d86fc
robq4290 <robert.quigley.429@gmail.com>
2022-01-06 22:17:39 +0000

    updated db_create_table to use dot dot dot s.t. any number of inputs can be used for a composite key. Obviously, this would need some control based off of the size of the table and constraints on the number of PKs allowed.
    Tested the output for both cases (no args, several args)
    
    Next Step:
    Create the tables in rpdr.sqlite and then write data into them

--- Functions/db_helper_functions.R
+++ Functions/db_helper_functions.R
@@ -1,34 +1,30 @@
-db_create_table <- function(db_conn, file_location, file_name, df_in, tbl_name,pk_val){
+db_create_table <- function(db_conn, file_location, file_name, df_in, tbl_name,...){
+  
+  sql_file <- readr::read_file(file=here::here(file_location,file_name))
   
   col_names <- DBI::SQL(colnames(df_in))
   
   col_names_sql <- glue::glue_sql("{col_names*}",.con=db_conn)
   
-  sql_file <- readr::read_file(file=here::here(file_location,file_name))
-  
-  if(length(pk_val)==1){
-    pk_val_sql
+  addn_args <- list(...)
+
+  pk_len <- length(addn_args)  
+  if(pk_len==0){
     
-    sql_param_statement <- DBI::sqlInterpolate(  conn=db_conn
-                                                 , sql=sql_file
-                                                 , table_name=DBI::SQL(tbl_name)
-                                                 , column_names=col_names_sql
-                                                 , primary_key_cols=DBI::SQL(pk_val)
-    )
-    sql_param_statement
+    pk_vals_sql <- DBI::SQL("NULL")
+
   }else{
     
-    pk_vals <- DBI::SQL(pk_val) 
+    pk_vals <- DBI::SQL(unlist(addn_args))
     
     pk_vals_sql <- glue::glue_sql("{pk_vals*}",.con=db_conn)
-    sql_param_statement <- DBI::sqlInterpolate(  conn=db_conn
-                                                 , sql=sql_file
-                                                 , table_name=DBI::SQL(tbl_name)
-                                                 , column_names=col_names_sql
-                                                 , primary_key_cols=pk_vals_sql
-    )
-    sql_param_statement
   }
   
-
+  sql_param_statement <- DBI::sqlInterpolate(  conn=db_conn
+                                               , sql=sql_file
+                                               , table_name=DBI::SQL(tbl_name)
+                                               , column_names=col_names_sql
+                                               , primary_key_cols=pk_vals_sql
+  )
+  sql_param_statement
 }
