f4d2180b9b1fafddcb0e5da3c7d9c5191e02a0d2
robq4290 <robert.quigley.429@gmail.com>
2022-01-07 04:33:56 +0000

    removed the chunks that get the names from the orig tables and then  re-arranged the columns in the table def... using the get_df_col_names_selectable made writing the query so much easier- no need to type out column names or manually indent!
    I noticed that I made a mistake when writing the initial three tables- the dates weren't cast correctly. I thought, very very very wrongly, that using the df that column type would be perserved but the dates are messy... this is why I should have used a ddl but oh well, only two to fix.
    
    All other columns seem to have the correct data type

--- SQL/rpdr_final_table_defn.sql
+++ SQL/rpdr_final_table_defn.sql
@@ -1,43 +1,37 @@
 select 
-  cntstnt_ep.season
-, cntstnt_ep.rank
-, cntstnt_ep.missc
-, cntstnt_ep.contestant
-, cntstnt_ep.episode
-, cntstnt_ep.outcome
-, cntstnt_ep.eliminated
-, cntstnt_ep.participant
-, cntstnt_ep.minichalw
-, cntstnt_ep.finale
-, cntstnt_ep.penultimate
-, cntstnt.season
-, cntstnt.contestant
-, cntstnt.age
-, cntstnt.dob
-, cntstnt.hometown
-, episds.season
-, episds.episode
-, episds.airdate
-, episds.special
-, episds.finale
-, episds.nickname
-, episds.runwaytheme
-, episds.numqueens
-, episds.minic
-, episds.minicw1
-, episds.minicw2
-, episds.minicw3
-, episds.bottom1
-, episds.bottom2
-, episds.bottom3
-, episds.bottom4
-, episds.bottom5
-, episds.bottom6
-, episds.lipsyncartist
-, episds.lipsyncsong
-, episds.eliminated1
-, episds.eliminated2
-
+    cntstnt_ep.season
+  , cntstnt_ep.episode
+  , strftime("%m-%d-%Y",episds.airdate, 'unixepoch')--strftime("%m-%d-%Y", date_col, 'unixepoch')
+  , episds.nickname
+  , episds.special
+  , cntstnt_ep.penultimate
+  , cntstnt_ep.finale
+  , episds.minic
+  , episds.runwaytheme
+  , episds.lipsyncartist
+  , episds.lipsyncsong
+  , cntstnt_ep.contestant
+  , cntstnt_ep.rank
+  , cntstnt_ep.missc
+  , cntstnt.age
+  , cntstnt.dob
+  , cntstnt.hometown
+  , cntstnt_ep.outcome
+  , cntstnt_ep.minichalw
+  , episds.minicw1
+  , episds.minicw2
+  , episds.minicw3
+  , episds.bottom1
+  , episds.bottom2
+  , episds.bottom3
+  , episds.bottom4
+  , episds.bottom5
+  , episds.bottom6
+  , episds.eliminated1
+  , episds.eliminated2
+  , cntstnt_ep.eliminated
+  , cntstnt_ep.participant
+  , episds.numqueens
 FROM cntstnt_ep
 
 LEFT JOIN cntstnt
@@ -51,5 +45,3 @@ LEFT JOIN episds
   ON cntstnt_ep.season=episds.season
   
   AND cntstnt_ep.episode=episds.episode
-  
-limit 1
